6efc3ca1f3912bc80c339713780bc70e9025d6dd
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2026-03-05 23:37:56 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ function App() {
 createRoot(document.getElementById("root")).render(
   <LoadingProvider>
     <AuthProvider>
+      {console.log("GOOGLE CLIENT ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID)}
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
