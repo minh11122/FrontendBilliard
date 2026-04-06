de204cd3f342144efde2378016a18be408907e67
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2026-04-06 20:14:33 +0700

    update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { loadingEmitter } from "@/utils/loadingEmitter";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:9999/api";
+const baseURL = import.meta.env.VITE_API_URL || "https://backendbilliard.onrender.com/api/";
 
 const api = axios.create({
   baseURL,
