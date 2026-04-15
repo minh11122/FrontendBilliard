3f130147721ef790c1aa2d372f480c9b08fb34c9
QuangDuy <144219131+Quangtd1911@users.noreply.github.com>
2026-04-15 22:22:42 +0700

    fix cancel tournament

--- src/pages/owner-page/owner-tournament-list-page.jsx
+++ src/pages/owner-page/owner-tournament-list-page.jsx
@@ -8,7 +8,8 @@ import {
   closeRegistration, 
   generateBracket, 
   startTournament, 
-  deleteTournament 
+  deleteTournament,
+  cancelTournament 
 } from "@/services/tournament.service";
 
 const tabs = [
