72de80dbbbffbaac329565bda5fd8f9ba1bf04d2
QuangDuy <144219131+Quangtd1911@users.noreply.github.com>
2026-05-29 20:15:34 +0700

    modify folder owner page

--- src/pages/owner-page/index.js
+++ src/pages/owner-page/index.js
@@ -1,33 +1,49 @@
-import { HeaderHome } from "@/components/layouts/home-layout/header-home";
-
-export { default as OwnerTableListPage } from "./owner-table-list-page";
-export { default as OwnerCreateTablePage } from "./owner-create-table-page";
-export * from "./owner-dashboard-page";
+// ---- Register Owner Account ----
 export * from "./register-owner-account/basic-information";
-export * from "./setting-page";
-export { default as PaymentSuccessPage } from "./PaymentSuccessPage";
-export { default as OwnerSelectClubPage } from "./owner-select-club-page";
-export { default as OwnerEditTablePage } from "./owner-edit-table-page";
-export { default as OwnerServiceListPage } from "./owner-service-list-page";
-export { default as OwnerCreateServicePage } from "./owner-create-service-page";
-export { default as OwnerEditServicePage } from "./owner-edit-service-page";
-export { default as OwnerListEmployeePage } from "./owner-list-employee-page";
-export { default as OwnerCreateEmployeePage } from "./owner-create-employee-page";
-export { default as OwnerUpdateEmployeePage } from "./owner-update-employee-page";
-export * from "./register-owner-account/basic-information";
-export * from "./setting-page";
-export { default as AmenitiesPage } from "./amenities-page";
-export * from "./owner-resubmit-club-page";
-export { default as OwnerOnboardingPage } from "./owner-onboarding-page";
-export { default as OwnerTournamentListPage } from "./owner-tournament-list-page";
-export { default as OwnerCreateTournamentPage } from "./owner-create-tournament-page";
-export { default as OwnerEditTournamentPage } from "./owner-edit-tournament-page";
-export { default as OwnerTournamentPlayersPage } from "./owner-tournament-players-page";
-export { default as OwnerReportsPage } from "./owner-reports-page";
-export { default as OwnerReviewListPage } from "./owner-review-list-page";
-export * from "./owner-tournament-bracket-page";
-export * from "./owner-post-page";
-export { default as OwnerPaymentHistoryPage } from "./owner-payment-history-page";
-export { default as OwnerTournamentDetailPage } from "./owner-tournament-detail-page";
 
-export { default as OwnerRegisterClubPage } from "./owner-register-club-page";
+// ---- Club ----
+export { default as OwnerOnboardingPage } from "./club/owner-onboarding-page";
+export { default as OwnerRegisterClubPage } from "./club/owner-register-club-page";
+export * from "./club/owner-resubmit-club-page";
+export { default as OwnerSelectClubPage } from "./club/owner-select-club-page";
+
+// ---- Table ----
+export { default as OwnerTableListPage } from "./table/owner-table-list-page";
+export { default as OwnerCreateTablePage } from "./table/owner-create-table-page";
+export { default as OwnerEditTablePage } from "./table/owner-edit-table-page";
+
+// ---- Service ----
+export { default as OwnerServiceListPage } from "./service/owner-service-list-page";
+export { default as OwnerCreateServicePage } from "./service/owner-create-service-page";
+export { default as OwnerEditServicePage } from "./service/owner-edit-service-page";
+
+// ---- Employee ----
+export { default as OwnerListEmployeePage } from "./employee/owner-list-employee-page";
+export { default as OwnerCreateEmployeePage } from "./employee/owner-create-employee-page";
+export { default as OwnerUpdateEmployeePage } from "./employee/owner-update-employee-page";
+
+// ---- Tournament ----
+export { default as OwnerTournamentListPage } from "./tournament/owner-tournament-list-page";
+export { default as OwnerCreateTournamentPage } from "./tournament/owner-create-tournament-page";
+export { default as OwnerEditTournamentPage } from "./tournament/owner-edit-tournament-page";
+export { default as OwnerTournamentPlayersPage } from "./tournament/owner-tournament-players-page";
+export { default as OwnerTournamentDetailPage } from "./tournament/owner-tournament-detail-page";
+export * from "./tournament/owner-tournament-bracket-page";
+
+// ---- Dashboard ----
+export * from "./dashboard/owner-dashboard-page";
+export { default as OwnerReportsPage } from "./dashboard/owner-reports-page";
+
+// ---- Finance ----
+export { default as OwnerPaymentHistoryPage } from "./finance/owner-payment-history-page";
+export { default as PaymentSuccessPage } from "./finance/PaymentSuccessPage";
+
+// ---- Setting ----
+export * from "./setting/setting-page";
+export { default as AmenitiesPage } from "./setting/amenities-page";
+
+// ---- Feedback ----
+export { default as OwnerReviewListPage } from "./feedback/owner-review-list-page";
+
+// ---- Post ----
+export * from "./post/owner-post-page";
